6e63c078db9897bd40c7bfe86b281ece2dcf80a3
Igor Tsiglyar <igor_tsiglyar@outlook.com>
2019-04-30 12:56:03 +0300

    add table creation

--- src/main/java/io/tsiglyar/github/repository/suggester/CassandraRepositoryPersister.java
+++ src/main/java/io/tsiglyar/github/repository/suggester/CassandraRepositoryPersister.java
@@ -1,15 +1,18 @@
 package io.tsiglyar.github.repository.suggester;
 
 import com.datastax.driver.core.RegularStatement;
+import com.datastax.driver.core.schemabuilder.SchemaBuilder;
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.tsiglyar.github.Repository;
+import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.cassandra.CassandraClient;
 import io.vertx.reactivex.cassandra.CassandraRowStream;
 import io.vertx.reactivex.core.Vertx;
 
 import java.util.List;
 
+import static com.datastax.driver.core.DataType.text;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.batch;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.insertInto;
@@ -28,12 +31,34 @@ public class CassandraRepositoryPersister implements RepositoryPersister {
   public Flowable<Repository> load(String language) {
     return client.rxQueryStream(select()
       .all()
-      .from("repositories")
+      .from("github", "repositories")
       .where(eq("language", language))
     )
       .flatMapPublisher(CassandraRowStream::toFlowable)
-      .map(row -> new Repository(row.getString("name"),
-        row.getString("description"), row.getString("url")));
+      .onErrorResumeNext(createKeyspace()
+        .andThen(createTable())
+        .andThen(Flowable.empty()))
+      .map(row -> new Repository(row.getString("name"), row.getString("description"), row.getString("url")));
+  }
+
+  private Completable createKeyspace() {
+    return client.rxExecute(SchemaBuilder.createKeyspace("github")
+      .with()
+      .replication(new JsonObject()
+        .put("class", "SimpleStrategy")
+        .put("replication_factor", 1)
+        .getMap()))
+      .ignoreElement();
+  }
+
+  private Completable createTable() {
+    return client.rxExecute(SchemaBuilder.createTable("repositories")
+      .addPartitionKey("name", text())
+      .addStaticColumn("description", text())
+      .addStaticColumn("url", text())
+      .addStaticColumn("language", text())
+    )
+      .ignoreElement();
   }
 
   @Override
