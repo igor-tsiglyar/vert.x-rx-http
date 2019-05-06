73d7d864286063c72c9ef5af3a7b9ea394378904
Igor Tsiglyar <igor_tsiglyar@outlook.com>
2019-05-06 03:19:02 +0300

    remove non-working backpressure

--- src/main/java/io/tsiglyar/github/repository/suggester/GithubRepositorySuggesterVerticle.java
+++ src/main/java/io/tsiglyar/github/repository/suggester/GithubRepositorySuggesterVerticle.java
@@ -2,10 +2,10 @@ package io.tsiglyar.github.repository.suggester;
 
 import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableConverter;
 import io.reactivex.FlowableTransformer;
+import io.reactivex.Scheduler;
 import io.reactivex.Single;
 import io.reactivex.functions.Consumer;
 import io.tsiglyar.github.Repository;
@@ -15,6 +15,7 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.RxHelper;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.MultiMap;
 import io.vertx.reactivex.core.http.HttpServerRequest;
@@ -29,43 +30,42 @@ public class GithubRepositorySuggesterVerticle extends AbstractVerticle {
 
   private GithubAdapter adapter;
   private RepositoryPersister persister;
+  private Scheduler scheduler;
 
   @Override
   public void init(Vertx vertx, Context context) {
     super.init(vertx, context);
     this.adapter = new GithubGraphQLApiAdapter(this.vertx);
     this.persister = new MongoDbRepositoryPersister(this.vertx);
+    this.scheduler = RxHelper.scheduler(vertx);
   }
 
   @Override
-  public Completable rxStart() {
+  public void start() {
     Router router = Router.router(vertx);
     router.route("/projects_to_contribute")
       .handler(HTTPRequestValidationHandler.create()
         .addQueryParam("language", GENERIC_STRING, true)
         .addQueryParam("cache", BOOL, false))
-      .handler(context -> Single.just(context.request().getParam("language"))
-        .flatMapPublisher(language -> persister.load(language)
-          .switchIfEmpty(Flowable.fromPublisher(adapter.getRepositoriesToContribute(language))))
-        .compose(cache(context))
-        .as(asJsonArray())
-        .doOnError(Throwable::printStackTrace)
-        .subscribe(
-          repos -> respond(HttpResponseStatus.OK, repos.encodePrettily()).accept(context.request()),
-          error -> respond(error).accept(context.request())
-        )
+      .handler(context -> {
+        System.out.println(Thread.currentThread().getName());
+        Single.just(context.request().getParam("language"))
+            .flatMapPublisher(language -> persister.load(language)
+              .switchIfEmpty(Flowable.fromPublisher(adapter.getRepositoriesToContribute(language))))
+            .compose(cache(context))
+            .as(asJsonArray())
+            .doOnError(Throwable::printStackTrace)
+            .subscribeOn(scheduler)
+            .subscribe(
+              repos -> respond(HttpResponseStatus.OK, repos.encodePrettily()).accept(context.request()),
+              error -> respond(error).accept(context.request())
+            );
+        }
       );
 
-    return Single.fromCallable(vertx::createHttpServer)
-      .flatMapCompletable(server -> server
-        .requestHandler(router)
-        .requestStream()
-        .toFlowable()
-        .onBackpressureDrop(respond(new RateLimitExceededException()))
-        .ignoreElements()
-        .andThen(server.rxListen(8080))
-        .ignoreElement()
-      );
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(8080);
   }
 
   private Consumer<HttpServerRequest> respond(HttpResponseStatus status, String content) {
